5f95b69a4216383b36d77378181ba15a54193ae1
gopae02 <gopae02@gmail.com>
2017-01-20 00:14:03 +0900

    17/01/20 - DP test. but need more understanding this logic

--- AlgorithmPractice/src/com/amikth/algoEx03/TestDynamicMakeOneTopDown.java
+++ AlgorithmPractice/src/com/amikth/algoEx03/TestDynamicMakeOneTopDown.java
@@ -33,12 +33,40 @@ import java.util.Scanner;
 public class TestDynamicMakeOneTopDown {
 	
 	public static int[] d;
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
 		d = new int[n+1];
+		System.out.println(RtnCntMakeOne(n));
+		
+	} 
+	
+	public static int RtnCntMakeOne(int n) {
+		
+		if(n==1) {
+			return 0;
+		}
+		if(d[n] > 0 ) {
+			return d[n];
+		}
+		
+		d[n] = RtnCntMakeOne(n-1) +1;
+		if(n%2==0) {
+			int temp = RtnCntMakeOne(n/2)+ 1;
+			if(d[n] > temp) {
+				d[n] = temp;
+			}
+		}
+		
+		if(n%3==0) {
+			int temp = RtnCntMakeOne(n/3) + 1;
+			if(d[n] > temp) {
+				d[n] = temp;
+			}
+		}
+		
+		return d[n];
 	}
 
 }
